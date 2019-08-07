4a0e060c5c35eed26f4b6e0619f8034aba48a04b
huangsh <hsh-me@outlook.com>
2019-08-07 15:50:35 +0800

    add entry points

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 from setuptools import setup
 
 setup(name='raa-assess',
-      version='0.1',
-      description='reduce amino acid assess',
-      url='https://github.com/huang-sh/raa-assess/',
-      author='huangsh',
-      author_email='hsh-me@outlook.com',
-      license='MIT',
-      packages=['raa-assess'],
-      install_requires=[
+    version='0.1',
+    description='reduce amino acid assess',
+    url='https://github.com/huang-sh/raa-assess/',
+    author='huangsh',
+    author_email='hsh-me@outlook.com',
+    license='MIT',
+    packages=['raa-assess'],
+    install_requires=[
         'numpy>=1.16.2',
         'matplotlib>=3.0.3',
         'scikit-learn>=0.21.2',
         'seaborn>=0.9.0',
         
         ],
-      python_requires=">=3.5",
-      include_package_data=True,
-      zip_safe=True)
+    entry_points={
+        'console_scripts': [
+        'raa = raa-assess.__main__:command_parser',
+            ]
+        },
+    python_requires=">=3.5",
+    include_package_data=True,
+    zip_safe=True)
