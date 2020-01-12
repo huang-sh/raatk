03ca2723b0e10113c824178eececed43a3071330
huangsh <hsh-me@outlook.com>
2020-01-12 21:27:56 +0800

    Update classify.py

--- raa_assess/classify.py
+++ raa_assess/classify.py
@@ -56,7 +56,7 @@ class Evaluate:
         return k_mean_metric, None, None 
 
     def holdout(self, test_size):
-        x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, test_size=test_size)
+        x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, test_size=test_size, random_state=1)
         fit_clf = self.model.fit(x_train, y_train)
         y_true, y_pre = y_test, fit_clf.predict(x_test)
         metric = self.metrics_(y_true, y_pre)
